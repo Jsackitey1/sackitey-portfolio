f092f4f934040b001b3c004a8462cd187a44641c
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2025-03-04 13:36:20 -0500

    diff

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
 import './App.css'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import LandingPage from './components/LandingPage'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import AboutPage from './components/AboutPage'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import Experience from './components/Experience'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import Fproject from './components/Fproject'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import Sproject from './components/Sproject'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import TechStacks from './components/TechStacks'
-// @ts-ignore
+// @ts-expect-error - JSX component without TypeScript definitions
 import Footer from './components/Footer'
 
 function App() {
